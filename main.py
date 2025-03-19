e52dc7fe6d722e5c74efef9ba28dc3d7396ef11a
amen <fikruamen11@gmail.com>
2025-03-19 23:15:50 +0300

    serialized with marshal

--- main.py
+++ main.py
@@ -1,30 +1,55 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse,abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+
+class VideoModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    views = db.Column(db.Integer, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"video(name={self.name},views={self.views},likes={self.likes})"
+
 
 Video_put_args = reqparse.RequestParser()
-Video_put_args.add_argument("name", type=str, help="name of video"),required=True
-Video_put_args.add_argument("views", type=int, help="view of video",required=True)
-Video_put_args.add_argument("likes", type=int, help="likes of video",required=True)
+Video_put_args.add_argument("name", type=str, help="name of video", required=True)
+Video_put_args.add_argument("views", type=int, help="view of video", required=True)
+Video_put_args.add_argument("likes", type=int, help="likes of video", required=True)
 
-videos = {}
-
-def if_id_not_found(video_id):
-    if video_id not in videos:
-        abort(404,message='id not found')
+resource_fields = {
+    "id": fields.Integer,
+    "name": fields.String,
+    "views": fields.Integer,
+    "likes": fields.Integer,
+}
 
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        if_id_not_found(video_id)
-        return videos[video_id]
+        result = VideoModel.query.get(id=video_id)
+        return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        args=Video_put_args.parse_args()
-        videos[video_id]=args
-        return videos[video_id],201
+        args = Video_put_args.parse_args()
+        video = VideoModel(
+            id=video_id, name=args["name"], views=args["views"], likes=args["likes"]
+        )
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
+    def delete(self, video_id):
+        del videos[video_id]
+        return "", 204
 
 
 api.add_resource(Video, "/video/<int:video_id>")
